a8f3d8db61aaddf8e633e45f5f232f1dad013e57
ojha-url <ojha@mit.edu>
2017-05-23 15:31:22 -0400

    Updated docker scripts

--- deploy/scripts/insideWaitForPsql.sh
+++ deploy/scripts/insideWaitForPsql.sh
@@ -3,6 +3,6 @@
 #wait until postgres is up
 while ! nc -z localhost 5432; do sleep 3; done
 
-createdb -h localhost -U skanda-user -p 5432 -T template0 housing_survey
-psql -h localhost -U skanda-user -p 5432 housing_survey < ./skanda_schema.sql && \
-      psql -h localhost -U skanda-user -p 5432 housing_survey < ./skanda_data.sql
+createdb -h localhost -U skanda -p 5432 -T template0 housing_survey
+psql -h localhost -U skanda -p 5432 housing_survey < ./skanda_schema.sql && \
+      psql -h localhost -U skanda -p 5432 housing_survey < ./skanda_data.sql
